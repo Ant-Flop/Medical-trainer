68f9efdaa0982682f89a276d3ec25cdc91de0b9d
Antony <antfloppy@gmail.com>
2021-11-25 08:51:17 +0200

    First commit

---
+++ medical-trainer-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import SignIn from "./Components/SignIn";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <SignIn />
+    </div>
+  );
+}
+
+export default App;
